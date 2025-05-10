c7fac3128d5f3065044d392c98448da18246d533
Shanmukh <kondan1120@gmail.com>
2025-05-10 16:01:00 +0530

    mongoDB setup

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,13 @@
 const express = require("express")
 const cors = require("cors")
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("MongoDB connected"))
+.catch((err) => console.error("MongoDB connection error:", err));
 
 const app = express();
 app.use(express.json())
